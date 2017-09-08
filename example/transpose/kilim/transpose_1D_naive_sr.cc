dc4a603bde6b41a6466818d689598da036613381
Jason Spencer <jason@jasonspencer.org>
2017-09-08 15:55:47 +0100

    removed old files

--- example/transpose/kilim/transpose_1D_naive_sr.cc
+++
@@ -1,7 +0,0 @@
-
-// naive transpose by direct copy with strength reduction
-template < typename T > void transpose_1D_naive_sr( const T * const in, T * out, const unsigned w ) {
-	for(unsigned i=0, iw=0;i<w;i++, iw+=w)
-		for(unsigned j=0, jw=0;j<w;j++, jw+=w)
-			out[jw+i]=in[iw+j];
-}
--- example/transpose/kilim/transpose_2D_naive.cc
+++
@@ -1,6 +0,0 @@
-
-template < typename T > void transpose_2D_naive( const T * const * const in, T ** out, const unsigned w ) {
-	for(unsigned i=0;i<w;i++)
-		for(unsigned j=0;j<w;j++)
-			out[j][i]=in[i][j];
-}
